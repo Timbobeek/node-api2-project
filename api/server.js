e951399cfc3dadf0e17d236274416000b4b63957
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-08 17:19:06 -0600

    first getter working

--- api/server.js
+++ api/server.js
@@ -1,5 +1,26 @@
 // implement your server here
 // require your posts router and connect it here
 
+const express = require('express');
 
-//start here
+const server = express();
+
+server.use(express.json());
+
+const Post = require('./posts/posts-model');
+
+server.get('/api/posts', (req, res) => {
+  Post.find(req.query)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "The posts information could not be retrieved",
+      })
+    })
+})
+
+module.exports = server;
+
